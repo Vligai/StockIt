ba978c6a5740488eab10f8e1fc66942f30b4acf3
vligai <vligai@stevens.edu>
2017-11-18 22:30:46 -0500

    added some really bad code

---
+++ src/main.js
@@ -0,0 +1,28 @@
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function dropdown1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+function dropdown2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
