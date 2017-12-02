c69feeef1ba09fcea93e7dc14ea59adb7e86403f
austin <austin.cawley@gmail.com>
2017-12-02 14:22:08 -0500

    Adding portfolio stocks
    Signed-off-by: austin <austin.cawley@gmail.com>

---
+++ public/js/loggedin.js
@@ -0,0 +1,160 @@
+function isDropdownContentClick(event) {
+  for (let p of event.path) {
+    if (p.className && p.className.includes("dropdown-content")) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function toggleDropDown(event) {
+  if (isDropdownContentClick(event)) {
+    return;
+  }
+  $(event.currentTarget).toggleClass("closed");
+}
+
+function makeStockHTML(data) {
+  let iconClass = data["Recommendation"].toLowerCase();
+
+  return `<div class="dropdown result closed" onclick="toggleDropDown(event)">
+            <section class="content">
+                <div class="icon ${iconClass}"></div>
+                <p> ${data["Ticker"]} </p>
+                <p> $${data["Current Price"]} </p>
+                <div class="btn-dropdown">
+                    <p><strong>${data["Recommendation"]}</strong></p>
+                    <div class="icon"></div>
+                </div>
+            </section>
+            <section class="dropdown-content">
+                <article class="data">
+                    <h3 class="title">Stats</h3>
+                    <p class="data-row">
+                        <span class="data-head">Current Volume:</span>
+                        ${data["Current Volume"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">90-Day Volume:</span>
+                        ${data["90-day Volume"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">Relative Volume:</span>
+                        ${data["Relative Volume"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">RSI:</span>
+                        ${data["RSI"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">50-day MA:</span>
+                        $${data["50-day MA"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">200-day MA:</span>
+                        $${data["200-day MA"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">Price Target:</span>
+                        $${data["Price Target"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">Time Horizon:</span>
+                        ${data["Time Horizon"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">Stop Loss Level:</span>
+                        ${data["Stop Loss Level"]}
+                    </p>
+                    <p class="data-row">
+                        <span class="data-head">Risk Reward:</span>
+                        ${data["Risk:Reward"]}
+                    </p>
+                </article>
+                <article class="description">
+                    <h3 class="title">Why?</h3>
+                    <p class="text">
+                        Alibaba's earnings report came in above expectations, showing YOY growth of 40%. 'Single's Day'
+                        sales exceeded last year's figures by 30%, while outlooks and earnings forecasts continue to be
+                        upbeat for the name.
+                    </p>
+                </article>
+            </section>
+        </div>`;
+}
+
+function loadData(csv) {
+  return new Promise((resolve, reject) => {
+    Papa.parse(csv, {
+      dynamicTyping: true,
+      download: true,
+      header: true,
+      delimeter: ",",
+      complete(res) {
+        if (res.errors.length > 0) {
+          reject(res);
+        }
+        resolve(res);
+      }
+    })
+  });
+}
+
+const throttle = (function () {
+  let timer = 0;
+  return (callback, ms) => {
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+
+let recommendations = [];
+let filteredRecs = [];
+let portfolioStocks = [];
+let filteredPortfolioStocks = [];
+let searchBox;
+let lastSearch;
+let searchResultsContainer;
+let portfolioContainer;
+
+function init() {
+  searchResultsContainer = $("#results");
+  portfolioContainer = $("#portfolio-stocks");
+  searchBox = $(".search-box");
+  searchBox.keyup(() => throttle(search, 500));
+
+  loadData("/data/stocks.csv")
+    .then((results) => {
+      recommendations = results.data;
+      return recommendations;
+    }).then((recs) => {
+      renderData(recs, searchResultsContainer)
+  });
+
+  loadData("/data/portfolio-stocks.csv")
+    .then((results) => {
+      portfolioStocks = results.data;
+      return portfolioStocks;
+    }).then((stocks) => {
+    renderData(stocks, portfolioContainer)
+  });
+}
+
+function search() {
+  let searchText = searchBox.val();
+  if (searchText !== lastSearch) {
+    lastSearch = searchText;
+    filteredRecs = recommendations.filter((rec) => {
+      return rec["Ticker"].toLowerCase().includes(searchText.toLowerCase());
+    });
+    renderData(filteredRecs, searchResultsContainer);
+  }
+}
+
+function renderData(recs, container) {
+  container.empty();
+  for (let rec of recs) {
+    container.append(makeStockHTML(rec));
+  }
+}
